379d0538e0aa822b5987eec3dbaccf0380741cc4
Gabriel Caetano <gvcaetano@inf.ufsm.br>
2020-04-16 19:18:39 -0300

    refactoring some functions and finished break part

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,68 +1,108 @@
 import React, { Component } from 'react';
 import Clock from './components/Clock';
+import Alarm from '../alarm.mp3';
 
-/**
- * @todo GitHub!!!!!!!!
- * @todo Avoid timer from going bellow zero
- * @todo Alert user that the timer is done
- * @todo Breaks part
+/*
  * @todo Styles
  * @todo Custom times?
  * @todo Mobile App?
  * @todo DEPLOY
  */
 
-const INITIAL_TIME = 60*25;
+// temporary start values for test
+const WORK_TIME = 15;
+const BREAK_TIME = 5;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.alarm = new Audio(Alarm);
 
     this.state = {
-      time: INITIAL_TIME,
+      time: WORK_TIME,
       interval: null,
-      running: false
+      running: false,
+      isWork: true,
     };
   }
 
-  tick = () => {
-    this.setState(prevState => ({
-      time: prevState.time-1,
-    }));
-  }
+  // start the clock
+  runTimer = () => {
+    this.setState({
+      interval: setInterval(() => this.tick(), 1000),
+      running: true,
+    });
+  };
 
-  handleTimer() {
-    if (!this.state.running) {
-      this.setState({
-        interval: setInterval(()=>this.tick(), 1000),
-        running: true
-      });
-    } else {
-      clearInterval(this.state.interval);
-      this.setState({
-        running: false
-      });
-    }
-  }
-
-  reset() {
-    if (this.state.interval) {
-      clearInterval(this.state.interval);
-    }
+  // stop the clock
+  stopTimer = () => {
+    clearInterval(this.state.interval);
     this.setState({
       running: false,
-      time: INITIAL_TIME
     });
+  };
 
+  // change the period
+  workBreak = () => {
+    if (this.state.isWork) {
+      alert('Continue to break');
+      this.setState({
+        isWork: false,
+        time: BREAK_TIME,
+      });
+      this.runTimer();
+    } else {
+      alert('Continue to work');
+      this.setState({
+        isWork: true,
+        time: WORK_TIME,
+      });
+      this.runTimer();
+    }
+  };
+
+  // decrement timer
+  tick = () => {
+    if (this.state.time <= 0) {
+      this.alarm.play();
+      this.stopTimer();
+      this.workBreak();
+    } else {
+      this.setState((prevState) => ({
+        time: prevState.time - 1,
+      }));
+    }
+  };
+
+  // start/stop button logic
+  handleTimer() {
+    if (!this.state.running) {
+      this.alarm.play();
+      this.runTimer();
+    } else {
+      this.stopTimer();
+    }
+  }
+
+  // reset pomodoro
+  reset() {
+    if (this.state.running) {
+      this.stopTimer();
+    }
+    this.setState({
+      time: WORK_TIME,
+    });
   }
 
   render() {
     return (
       <div>
-        <Clock time={ this.state.time }/>
-        <button onClick={() => this.handleTimer()}>{this.state.running ? 'STOP' : 'START'}</button>
+        <Clock time={this.state.time} />
+        <button onClick={() => this.handleTimer()}>
+          {this.state.running ? 'STOP' : 'START'}
+        </button>
         <button onClick={() => this.reset()}>RESET</button>
       </div>
-    )
+    );
   }
 }
